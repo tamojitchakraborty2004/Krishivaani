bc55827e0f93a904f8034cd90339803d898a5606
tamojitchakraborty2004 <ctamojit1@gmail.com>
2025-05-07 11:52:59 +0530

    Port issue fixed

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 from app.twilio_webhook import app
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use 5000 locally, use $PORT on Render
+    app.run(host='0.0.0.0', port=port, debug=True)
